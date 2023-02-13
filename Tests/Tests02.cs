e8bf38c55831b3627ab31a2c2743e44b14451f0c
irenehl <26irenelopez@gmail.com>
2023-02-12 22:25:38 -0600

    feature: challenge 02

---
+++ Tests/Tests02.cs
@@ -0,0 +1,83 @@
+﻿using _02;
+
+namespace Tests;
+
+
+public class Tests02
+{
+    [SetUp]
+    public void Setup()
+    {
+        
+    }
+
+    [Test]
+    public void CheckParenthesis_ShouldFail_WhenStrStartsWithClosingParenthesis()
+    {
+        // aaa
+        // Arrange -> prepare test
+        var str = ")asaaa";
+
+        // Act -> execute action
+        var result = Program.CheckParenthesis(str);
+
+        // Assert -> verify (1)
+        Assert.That(result, Is.EqualTo(false));
+    }
+    
+    [Test]
+    public void CheckParenthesis_ShouldFail_WhenStrHasDifferentParenthesisAmount()
+    {
+        // aaa
+        // Arrange -> prepare test
+        var str = "(()";
+
+        // Act -> execute action
+        var result = Program.CheckParenthesis(str);
+
+        // Assert -> verify (1)
+        Assert.That(result, Is.EqualTo(false));
+    }
+    
+    [Test]
+    public void CheckParenthesis_ShouldSucceed_WhenStrHasSameParenthesisAmout()
+    {
+        // aaa
+        // Arrange -> prepare test
+        var str = "()()(())";
+
+        // Act -> execute action
+        var result = Program.CheckParenthesis(str);
+
+        // Assert -> verify (1)
+        Assert.That(result, Is.EqualTo(true));
+    }
+    
+    [Test]
+    public void CheckParenthesis_ShouldSucceed_WhenStrIsEmpty()
+    {
+        // aaa
+        // Arrange -> prepare test
+        var str = string.Empty;
+
+        // Act -> execute action
+        var result = Program.CheckParenthesis(str);
+
+        // Assert -> verify (1)
+        Assert.That(result, Is.EqualTo(true));
+    }
+    
+    [Test]
+    public void CheckParenthesis_ShouldSucceed_WhenStrHasNoParenthesis()
+    {
+        // aaa
+        // Arrange -> prepare test
+        var str = "asaaa";
+
+        // Act -> execute action
+        var result = Program.CheckParenthesis(str);
+
+        // Assert -> verify (1)
+        Assert.That(result, Is.EqualTo(true));
+    }
+}
--- _02/Program.cs
+++ _02/Program.cs
@@ -1,3 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿namespace _02
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
 
-Console.WriteLine("Hello, World!");
+        public static bool CheckParenthesis(string str)
+        {
+            var opened = 0;
+            var closed = 0;
+            
+            if (!str.Contains('(') && !str.Contains(')')) return true;
+
+            var startingFlag = true;
+            
+            foreach (var ch in str)
+            {
+                if (ch == ')' && startingFlag) return false;
+
+                if (ch == '(')
+                {
+                    opened++;
+                    startingFlag = false;
+                }
+                else if (ch == ')')
+                {
+                    closed++;
+                    startingFlag = false;
+                }
+            }
+
+            return opened == closed;
+        }
+    }
+}
