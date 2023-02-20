37211986b226e11a3630a11deb4af6bab8842c97
irenehl <26irenelopez@gmail.com>
2023-02-20 13:03:15 -0600

    feat: challenge 02

---
+++ W_02._02/User.cs
@@ -0,0 +1,25 @@
+﻿namespace W_02._02;
+
+public class User : WithErrors
+{
+    private int Age { get; set; }
+    
+    public User(int age)
+    {
+        Age = age;
+    }
+
+    public override bool IsValid()
+    {
+        if (Age >= 18) return true;
+        
+        Errors.Add("User is underage");
+        return false;
+
+    }
+
+    public override bool HasErrors()
+    {
+        return Errors.Count > 0;
+    }
+}
