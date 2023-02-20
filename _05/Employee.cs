63dd5e186c0611a4d554d8695ae3a0e447a3f7a8
irenehl <26irenelopez@gmail.com>
2023-02-20 10:52:57 -0600

    feat: challenge 05

---
+++ _05/Employee.cs
@@ -0,0 +1,19 @@
+﻿namespace _05;
+
+public class Employee
+{
+    // Property
+    private double Salary { get; set; }
+    
+    // Method
+    protected Employee(double salary)
+    {
+        Salary = salary;
+    }
+
+    // Constructor
+    public double GetPaymentAmount()
+    {
+        return Salary;
+    }
+}
---
+++ _05/Salesman.cs
@@ -0,0 +1,19 @@
+﻿namespace _05;
+
+public class Salesman : Employee
+{
+    // Properties
+    private double Commission { get; set; }
+
+    // Method
+    public new double GetPaymentAmount()
+    {
+        return base.GetPaymentAmount() + Commission;
+    }
+
+    // Constructor
+    public Salesman(double salary, double commission) : base(salary)
+    {
+        Commission = commission;
+    }
+}
